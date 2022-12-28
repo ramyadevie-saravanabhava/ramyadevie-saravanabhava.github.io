ca987ab028596b734fddbc1993fec4ffdf591a3f
yuujay <gowtham.uj@gmail.com>
2022-12-27 20:44:37 -0500

    change branch to deploy in gh pages

--- next.config.js
+++ next.config.js
@@ -1,23 +1,12 @@
-/** @type {import('next').NextConfig} */
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  reactStrictMode: true,
+/**
+   * @type {import('next').NextConfig}
+   */
+const nextConfig   = {
   images: {
-    loader: 'imgix',
-    path: 'the "domain" of your Imigix source',
+    loader: 'akamai',
+    path: '',
   },
-}
+  assetPrefix: './',
+};
+
+export default   nextConfig;
