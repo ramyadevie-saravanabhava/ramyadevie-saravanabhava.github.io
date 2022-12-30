76b896a814421c572082b5d64263982bbb9f916c
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2022-12-30 12:31:18 -0500

    Updated View mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import 'tailwindcss/tailwind.css'
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({Component, pageProps}) {
     return <>
