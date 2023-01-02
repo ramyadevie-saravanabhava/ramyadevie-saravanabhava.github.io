7b8d8a04ec03bb0bba782d958b661cb7742b1a2c
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-01-02 16:52:51 -0500

    Updated profile picture

--- components/About.js
+++ components/About.js
@@ -1,6 +1,4 @@
 import React from "react"
-// import Image from "next/image"
-// import profile from "../public/Coding.png"
 
 const About = () => {
     return (
@@ -8,20 +6,17 @@ const About = () => {
             <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
                 <div className="lg:px-4 lg:mt-12 ">
                     <h1 className="text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white">
-                        Hey there,
+                        Hey there, I'm Ramya
                     </h1>
                     <div className="mt-6 text-gray-800 dark:text-white">
                         <p className="mb-4">
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and scrambled it to make a
-                            type specimen book.
+                            An aspiring Passionate full stack web Developer
                         </p>
                     </div>
                 </div>
-                {/* <div className="flex-shrink-0 lg:mt-12 lg:px-4 mb-10">
-                    <Image
-                        src={profile}
+                <div className="flex-shrink-0 lg:mt-12 lg:px-4 mb-10">
+                    <img
+                        src="https://drive.google.com/uc?export=view&id=1YmgOk2-FB6rLvcij4uogfp1B25XJypNZ"
                         alt="Profile"
                         priority={true}
                         className="rounded-full"
@@ -29,7 +24,7 @@ const About = () => {
                         height={250}
                         placeholder="blur"
                     />
-                </div> */}
+                </div>
             </div>
         </div>
     )
--- components/Footer.js
+++ components/Footer.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div className="lg:mt-18 sm:py-12 py-6">
-            <div className="max-w-4xl px-4 mx-auto text-gray-800 dark:text-white">
-                <div className="pb-8 mb-2 border-t-2 border-gray-300 dark:border-white-300"></div>
-                <div className="flex flex-col justify-between lg:flex-row items-center">
-                    <p>Built with Next.js, Tailwind and Vercel</p>
-                </div>
-            </div>
-        </div>
+        <footer className="fixed bottom-0 left-0 z-20 p-4 w-full bg-white border-t border-gray-200 shadow md:items-center md:justify-between md:p-6 dark:bg-black dark:border-gray-600">
+            {/* <div className="max-w-4xl px-4 mx-auto text-gray-800 dark:text-white">
+                <div className="pb-8 mb-2 border-t-2 border-gray-300 dark:border-white-300"></div> */}
+                {/* <div className="flex flex-col justify-between lg:flex-row items-center"> */}
+                    <p className="text-center">Built with Next.js, Tailwind, Vercel and ♥️</p>
+                {/* </div> */}
+            {/* </div> */}
+        </footer>
     )
 }
 
