f44991735f7dfa796bb9aa3f42358dfc3613d487
ramyadevie-saravanabhava <113299370+ramyadevie-saravanabhava@users.noreply.github.com>
2024-05-20 16:09:02 +0000

    Deploying to gh-pages from @ ramyadevie-saravanabhava/ramyadevie-saravanabhava.github.io@62295ab226043b5bd8a10b90eba423a2e071d7ca 🚀

---
+++ _next/static/1Fh5o4HtmlLZJA21b1lJM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/574084a258343374.css","static/chunks/pages/index-c0af3d1aa1d50773.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
