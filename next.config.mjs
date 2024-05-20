4c9bcc8712ddedccf2836d896c43c187ac4ce4ff
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2024-05-19 23:32:29 -0400

    disable next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
-/**
-   * @type {import('next').NextConfig}
-   */
-const nextConfig   = {
-  // images: {
-  //   loader: 'akamai',
-  //   path: '',
-  // },
-  // assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || './',
-};
+// /**
+//    * @type {import('next').NextConfig}
+//    */
+// const nextConfig   = {
+  
+//   // images: {
+//   //   loader: 'akamai',
+//   //   path: '',
+//   // },
+//   // assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || './',
+// };
 
-export default   nextConfig;
+// export default   nextConfig;
