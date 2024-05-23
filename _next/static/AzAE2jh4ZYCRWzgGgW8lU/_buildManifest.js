57b8f41750adbdc76b35d5ca6aa73da48cab386f
ramyadevie-saravanabhava <113299370+ramyadevie-saravanabhava@users.noreply.github.com>
2024-05-23 15:52:01 +0000

    Deploying to gh-pages from @ ramyadevie-saravanabhava/ramyadevie-saravanabhava.github.io@ab2a7b177d4947579a986de5dfb96067803fce63 🚀

--- _next/static/AzAE2jh4ZYCRWzgGgW8lU/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/574084a258343374.css","static/chunks/pages/index-0dd60c4a85dba946.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/xAp2YcwM6hAZYfHdxS6wG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/574084a258343374.css","static/chunks/pages/index-0557369f68ce64b8.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
